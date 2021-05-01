703a4807c2c54625580f14f481ba857eec27a074
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-05-01 17:07:02 +0300

    install morgan to log http requests

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 require('dotenv/config') // npm install --save dotenv yo add variables that will be used globally in all files inside the app
 var api = process.env.API_URL; // target the API_URL defined inside .env file
-app.use(express.json()); // this is called middleware, exprees will accept json data, this method allow our data to be understandable by express , which we are sent from front end
+app.use(express.json()); // this is called middleware, express will accept json data, this method allow our data to be understandable by express , which we are sent from front end
+app.use(morgan('tiny')); // this is library morgan npm install morgan used to log http requests in the console 
 app.get(`${api}/products`, (req, res) => {
     const product = {
         id: 1,
