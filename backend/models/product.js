6cc5011c401786c5388aa2b62fd7de2e2134d1ec
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-05-02 13:58:00 +0300

    Add Product Schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -2,11 +2,59 @@ const mongoose = require("mongoose");//mongoose is responsible for all operation
 
 // create schema for products tha contains it's fields
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStick: {
-        type: Number,
+    name: {
+        type: String,
         required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        default: '' // default value if not entered
+    },
+    image: {
+        type: String,
+        default: ''
+    },
+    images: [{ // images is array og images and type of each image is String
+        type: String,
+    }],
+    brand: {
+        type: String,
+        default: ''
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    category: { // 
+        type: mongoose.Schema.Types.ObjectId, // type is id of category
+        ref: 'Category', // refferrence to category Schema, to insure that the id above is connected to category schema
+        required: true
+    },
+    countInStock: {
+        type: Number,
+        required: true,
+        min: 0, // to prevent minus numbers
+        max: 255
+    },
+    rating: {
+        type: Number,
+        default: 0
+    },
+    numReviews: {
+        type: Number,
+        default: 0
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
     }
 })
 
