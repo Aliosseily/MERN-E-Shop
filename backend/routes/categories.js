0560da7416e2550de696b59af5f28fcf9919fb34
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-05-02 15:24:57 +0300

    update category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -14,7 +14,7 @@ router.get(`/:id`, async (req, res) => {
     const category = await Category.findById(req.params.id);
 
     if (!category) {
-        res.status(500).json({ success: false , message:"Category mot found!" })
+        res.status(500).json({ success: false, message: "Category mot found!" })
     }
     res.status(200).send(category);
 })
@@ -44,6 +44,28 @@ router.delete('/:id', async (req, res) => {
         return res.status(400).json({ success: false, error: err });
 
     }
+})
+
+router.put('/:id', async (req, res) => {
+    try{
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        {new : true} // this mean that I want to return the new updated data not the old one
+    )
+
+    if (!category) {
+        return res.status(404).send('The category cannot be updated!');
+    }
+    res.send(category);
+}
+catch(err){
+    console.log("ERRRRRRRRRRRRR" , err)
+}
 })
 
 module.exports = router;
