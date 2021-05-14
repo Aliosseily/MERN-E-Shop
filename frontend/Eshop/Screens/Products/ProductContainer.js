91d709bb9acc53f1db075c767b216450ea9ebf51
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-05-14 17:06:41 +0300

    Product Card render data and style

--- frontend/Eshop/Screens/Products/ProductContainer.js
+++ frontend/Eshop/Screens/Products/ProductContainer.js
@@ -1,18 +1,27 @@
 import React from 'react'
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import Products from '../../assets/data/products.json'
+import ProductList from './ProductList';
 
 const ProductContainer = () => {
-    return(
+    //numColumns={2} devide screen into 2 columns
+    return (
         <View>
-            <FlatList
-            data={Products}
-            keyExtractor={item => item.name}
-            renderItem={itemData =>
-            <Text>{itemData.item.brand}</Text>
-            }
-            />
+            <Text>Product Container</Text>
+            <View style={{ marginTop: 100 }}>
+                <FlatList
+                    numColumns={2}
+                    data={Products}
+                    keyExtractor={item => item.name}
+                    renderItem={item =>
+                        <ProductList
+                            key={item.id}
+                            item={item.item}
+                        />
+                    }
+                />
+            </View>
         </View>
     )
 }
---
+++ frontend/Eshop/Screens/Products/ProductList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { View, Text, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import ProductCard from './ProductCard';
+
+//Dimensions, allow us to get the dimensions of the device, so we can calculate dynamically our style
+var { width } = Dimensions.get("window");
+
+const ProductList = props => {
+    var { item } = props; // store every thing in props in item varaible (Destructuring)
+    // width / 2 to have two columns
+    // {...item} send every thing in props to ProductCart 
+    return (
+        <TouchableOpacity style={{ width: '50%' }}>
+            <View style={{ width: width / 2, backgroundColor: 'gainsboro' }}>
+                <ProductCard {...item} /> 
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default ProductList;
