50e6fdd37ad17e4ed225dd57e3fcc8a25d6f5887
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-05-15 14:01:24 +0300

    Use Native Base

--- frontend/Eshop/Screens/Products/ProductContainer.js
+++ frontend/Eshop/Screens/Products/ProductContainer.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Container, Header, Icon, Item, Input, Text } from 'native-base';
 
 import Products from '../../assets/data/products.json'
 import ProductList from './ProductList';
@@ -7,22 +8,30 @@ import ProductList from './ProductList';
 const ProductContainer = () => {
     //numColumns={2} devide screen into 2 columns
     return (
-        <View>
-            <Text>Product Container</Text>
-            <View style={{ marginTop: 100 }}>
-                <FlatList
-                    numColumns={2}
-                    data={Products}
-                    keyExtractor={item => item.name}
-                    renderItem={item =>
-                        <ProductList
-                            key={item.id}
-                            item={item.item}
-                        />
-                    }
-                />
+        <Container>
+            <Header searchBar rounded>
+                <Item>
+                    <Icon name="ios-search"/>
+                    <Input placeholder="search"/>
+                </Item>
+            </Header>
+            <View>
+                <Text>Product Container</Text>
+                <View style={{ marginTop: 100 }}>
+                    <FlatList
+                        numColumns={2}
+                        data={Products}
+                        keyExtractor={item => item.name}
+                        renderItem={item =>
+                            <ProductList
+                                key={item.id}
+                                item={item.item}
+                            />
+                        }
+                    />
+                </View>
             </View>
-        </View>
+        </Container>
     )
 }
 
