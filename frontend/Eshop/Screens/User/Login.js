32a9de6dc6b0e72df9af8fb6b8041cfb2b0a5b0b
Ali Ossaily <ali.ossaily@Almajmoua.org>
2021-06-28 12:49:09 +0300

    login handler with error component

--- frontend/Eshop/Screens/User/Login.js
+++ frontend/Eshop/Screens/User/Login.js
@@ -1,13 +1,71 @@
-import React from 'react';
-
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet,Button } from 'react-native';
+import FormContainer from '../../Shared/Forms/FormContainer';
+import Input from '../../Shared/Forms/Input';
+import Error from '../../Shared/error';
 
 const Login = props => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+
+const handleSubmit = () => {
+    const user = {
+        email,
+        password
+    }
+    if(email === ""  || password === "" ){
+        setError("Please fill in your credentials")
+    }
+    else{
+        console.log("Success")
+    }
+}
+
     return (
-        <View>
-            <Text>Login</Text>
-        </View>
+        <FormContainer title={"Login"}>
+            <Input
+                placeholder={"Enter Email"}
+                name={"Email"}
+                id={"Email"}
+                value={email}
+                onChangeText={text => setEmail(text.toLowerCase())}
+            />
+
+            <Input
+                placeholder={"Enter Password"}
+                name={"Password"}
+                id={"Password"}
+                value={password}
+                onChangeText={text => setPassword(text)}
+                secureTextEntry={true}
+            />
+
+            <View style={styles.buttonGroup}>
+                {error ? <Error message={error} /> : null }
+                <Button title="Login"  onPress={() => {handleSubmit()}}/>
+            </View>
+
+            <View style={[{ marginTop: 40 }, styles.buttonGroup]}>
+                <Text style={styles.middleText}>Don't have an account yet ?</Text>
+                <Button title="Register" onPress={() => props.navigation.navigate("Register")} />
+            </View>
+
+        </FormContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    buttonGroup: {
+        width: "80%",
+        alignItems: 'center'
+    },
+    middleText: {
+        marginBottom: 20,
+        alignSelf: 'center'
+    }
+})
+
 
 export default Login;
